6b0af64b556368ea8af002bd55ff751b1c9a5a56
mandoohi <cris0807@naver.com>
2022-04-30 17:57:57 +0900

    Initial commit

---
+++ lab_data/03.load_data.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+data = pd.read_csv('../data/titles.csv')
+print(data)
+
+titles = data['title'].values
+print(titles)
---
+++ main2.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+data = pd.read_csv('data/titles.csv')
+titles = data['title'].values
+word_index = dict()
+
+for title in titles:
+    words = title.split()
+    for word in words:
+        if word not in word_index:
+            word_index[word] = len(word_index)
+
+
+print(word_index)
